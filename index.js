677037ce50529a5da59959c40ee5908bd328226c
EduardoCarbajalM <ecarbajalm1801@alumno.ipn.mx>
2024-07-03 01:54:54 -0600

    agregar certificaciones principales, paqueteria office y flecha de desplazamiento

---
+++ index.js
@@ -0,0 +1,18 @@
+window.onscroll = function() {
+    var scrollToTop = document.getElementById('scrollToTop');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTop.style.display = "block";
+    } else {
+      scrollToTop.style.display = "none";
+    }
+  }
+  
+  function scrollToTop() {
+    var scrollToTop = document.getElementById('scrollToTop');
+    scrollToTop.classList.add('clicked');
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    setTimeout(function() {
+      scrollToTop.classList.remove('clicked');
+    }, 500);
+  }
+  
